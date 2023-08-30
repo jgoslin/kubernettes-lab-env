af11abb3cd53cfb63d16421cbed63dbc8610b563
John R. Goslin <john@goslin.net>
2023-08-30 18:09:43 -0400

    added new playbooks

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 #This is a repository for an ansible deployment of a kubernettes lab using ubuntu servers
+
+To use the run-playbook script create a .become password file.
+
+That the shell run the below command on the project folder
+echo "somesuper_secure_password" > .become
---
+++ run-playbook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the user has provided an argument
+if [ $# -eq 0 ]
+  then
+    echo "No palbook supplied"
+    exit 1
+fi
+
+# Assign the first argument to a variable
+playbook=$1
+
+# Execute the command with the argument
+echo "Executing playbook: $arg1"
+ansible-playbook -i inventory.yml $playbook --private-key ~/.ssh/ansible -u ansible --become-password-file .become
