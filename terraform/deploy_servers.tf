4d8eca870e90405b597bf6c7a72ef9408b049b9f
John R. Goslin <john@goslin.net>
2023-09-13 22:24:25 -0400

    added storage nodes

--- terraform/deploy_servers.tf
+++ terraform/deploy_servers.tf
@@ -8,55 +8,81 @@ variable "servers" {
   default = [
     {
       target = "pve1"
-      name = "k3s-master1"
+      name = "k3s-ctrl1"
       desc = "k3s master node"
       vmid = 8001
       ip   = "192.168.136.231/24"
+      virtio = "D6:BE:82:D5:FB:01"
       cores = 2
       memory = 2048
     },
     {
       target = "pve2"
-      name = "k3s-master2"
+      name = "k3s-ctrl2"
       desc = "k3s master node"
       vmid = 8002
       ip   = "192.168.136.232/24"
+      virtio = "D6:BE:82:D5:FB:02"
       cores = 2
       memory = 2048
     },
     {
-      target = "pve1"
-      name = "k3s-master3"
+      target  = "pve1"
+      name = "k3s-ctrl3"
       desc = "k3s master node"
       vmid = 8003
       ip   = "192.168.136.233/24"
+      virtio = "D6:BE:82:D5:FB:03"
       cores = 2
       memory = 2048
     },
     {
       target = "pve2"
-      name = "k3s-worker1"
+      name = "k3s-node1"
       desc = "k3s worker node"
       vmid = 8010
       ip   = "192.168.136.234/24"
+      virtio = "D6:BE:82:D5:FB:04"
       cores = 4
       memory = 4096
     },
     {
       target = "pve1"
-      name = "k3s-worker2"
+      name = "k3s-node2"
       desc = "k3s worker node"
       vmid = 8011
       ip   = "192.168.136.235/24"
+      virtio = "D6:BE:82:D5:FB:05"
       cores = 4
       memory = 4096
     },
     {
       target = "pve2"
-      name = "k3s-worker3"
+      name = "k3s-node3"
       desc = "k3s worker node"
       vmid = 8012
       ip   = "192.168.136.236/24"
+      virtio = "D6:BE:82:D5:FB:06"
+      cores = 4
+      memory = 4096
+    },
+    {
+      target = "pve1"
+      name = "k3s-store1"
+      desc = "k3s worker node"
+      vmid = 8101
+      ip   = "192.168.136.237/24"
+      virtio = "D6:BE:82:D5:FB:07"
+      cores = 4
+      memory = 4096
+    },
+    {
+      target = "pve2"
+      name = "k3s-store2"
+      desc = "k3s worker node"
+      vmid = 8102
+      ip   = "192.168.136.238/24"
+      virtio = "D6:BE:82:D5:FB:08"
       cores = 4
       memory = 4096
     },
@@ -77,16 +103,18 @@ resource "proxmox_vm_qemu" "servers" {
         os_type = "cloud-init"   
         cpu = "host"
         agent = 1
+        onboot = true
 
         # VM System Resource Settings
         sockets = 1
         cores = each.value.cores
-        memory = 2048
+        memory = each.value.memory
 
         # VM Network Settings
         network {
-            bridge = "vmbr0"
-            model  = "virtio"
+          bridge = "vmbr0"
+          macaddr = each.value.virtio
+          model  = "virtio"
         }
         
         ipconfig0 = "gw=192.168.136.1,ip=${each.value.ip}"
@@ -95,12 +123,12 @@ resource "proxmox_vm_qemu" "servers" {
 
         # VM Disk Settings  
         disk {
-        storage = "truenas-nfs"
-        size = "32G"
-        type = "scsi"
-        ssd = 1
-        discard = "on"
-        iothread = 1
+          storage = "truenas-nfs"
+          size = "32G"
+          type = "scsi"
+          ssd = 1
+          discard = "on"
+          iothread = 1
         }
 
         #VM Display Settings
